06a23c21808f1ea6b23a46f8286c1cdf86e52c14
Jose Beltran <lucasjose4521@gmail.com>
2023-10-06 14:00:58 -0500

    Creando autoclik para Jose

--- autoclic.py
+++ autoclic.py
@@ -1,23 +1,12 @@
 from pyautogui import *
 from tkinter import *
 from tkinter import messagebox as MessageBox
-import pyautogui
 import time
-import keyboard
-import random
 import win32api, win32con
 
 
-# pyautogui.displayMousePosition()
-# Position 888 X: 3406 y: 78 RGB (247,180,142)
-# Position reload X: 3016 y: 118 RGB (247,180,142)
-# Position blanco X: 2174 y: 1250
-# Position negro X: 2392 y: 437
-
-
-
-
-def click(x,y):
+#-*-*-*-*-*-*-*-*-*-*-*-*-*-* Funcion para el click *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+def click(x,y): 
     win32api.SetCursorPos((x,y)) #----------------------------------> Posicion del cursor
     time.sleep(0.5) #-----------------------------------------------> Tiempo antes de precionar el boton del mouse luego de posicionarse
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0) #-------> Boton precionado del mouse
@@ -29,19 +18,11 @@ def click(x,y):
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0) #---------> Boton suelto del mouse
 
 def temporizador():
-    # while keyboard.is_pressed('q') == False:
     while 2 == 2 :
-        time.sleep(10)
-        click(3416,80)
-        # print("Click 1 funcionando")
-        time.sleep(5)
-        click(3016,118)
-        # print("Click 2 funcionando")
-        # resultado = MessageBox.askquestion("Reiniciar", "¿Está seguro que desea reinciar?")
-        # if resultado == "yes":
-        #     time.sleep(0.01)
-        # else:
-        #     MessageBox.showwarning("Alerta", "Sección sólo para administradores.")
+        time.sleep(600)
+        click(5158,63)
+        time.sleep(1)
+        click(3926,97)
 
 def exitprog():
     root.destroy()
@@ -49,13 +30,27 @@ def exitprog():
 root = Tk()
 frame = Frame(root)
 frame.pack()
-frame.config(width=150,height=50)
-Button(root, text = "Iniciar", command=temporizador).pack()
-Button(root, text = "Cerrar", command=exitprog).pack()
+frame.config(width=60,height=50)
+
+Button(root, text = "Iniciar", command=temporizador).pack(side=LEFT)
+Button(root, text = "Cerrar", command=exitprog).pack(side=LEFT)
 
 
-root.wm_attributes("-topmost", True) # Esta es la línea importante.
+root.wm_attributes("-topmost", True) # Esta es la línea importante de superposicion de pantalla .
 root.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -79,3 +74,15 @@ root.mainloop()
         
         # click(3434,1024)
         # print("Click 2 funcionando")
+
+# -------------------------------------------- Desuso ------------------------------------#
+
+
+
+# pyautogui.displayMousePosition()
+
+        # j, k = pyautogui.position()
+        # MessageBox.showwarning("Alerta","j: {}, k: {}".format(j, k))
+        # break
+
+# pyinstaller --windowed --onefile --icon=./icono.ico autoclic.py
